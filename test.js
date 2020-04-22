a06a5c2a5bcca989e82c31938303d20710934d1b
Tabt43 <Tiabt15@g.holycross.edu>
2020-04-22 09:37:46 -0400

    my weeks worth of work

---
+++ test.js
@@ -0,0 +1,11 @@
+{
+    document.getElementById("button").onclick = function(){
+        document.getElementById("try").innerHTML = "New Basketball";
+    }
+}
+
+
+let myList = ['dog', 'cat','mouse', 'human']
+for (i = 0;i < myList.length;i++){
+    console.log(myList[i])
+}
